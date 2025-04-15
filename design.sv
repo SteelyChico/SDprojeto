61fa30ed9bac770df195f7af9841fa6704004a5c
João <102252484+SteelyChico@users.noreply.github.com>
2025-04-15 17:15:44 -0300

    Create design.sv

---
+++ design.sv
@@ -0,0 +1,8 @@
+`include "instruction_memory.sv"
+`include "control_unit.sv"
+`include "alu_control.sv"
+`include "register_file.sv"
+`include "alu.sv"
+`include "data_memory.sv"
+`include "sign_extend.sv"
+`include "mips_processor.sv"
